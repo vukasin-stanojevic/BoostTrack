2dc9c1ca0e00b5692d67a018658eeadeb8c5e9a7
vukasin-stanojevic <68564585+vukasin-stanojevic@users.noreply.github.com>
2024-02-23 15:20:16 +0100

    Update convert_mot17_to_coco.py

--- data/tools/convert_mot17_to_coco.py
+++ data/tools/convert_mot17_to_coco.py
@@ -5,7 +5,7 @@ import cv2
 
 
 # Use the same script for MOT16
-DATA_PATH = "datasets/mot"
+DATA_PATH = "data/MOT17"
 OUT_PATH = os.path.join(DATA_PATH, "annotations")
 SPLITS = [
     "train_half",
